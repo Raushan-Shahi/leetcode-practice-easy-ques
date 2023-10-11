d9d5b2350703cb598c8956221b470b42ae6688db
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-10-11 15:12:55 +0530

    bit manipulation -> number of steps required to reduce a number to 0

---
+++ bitmanip_num_step_to_zero.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+    
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int ans =0;
+        while (num>0)
+        {
+            if(num%2 ==0){
+                num /=2;
+                ans++;
+            }
+            else{
+                num -=1;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
