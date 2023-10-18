10b3b84ef672b91901d92d91e9fd5fc1d11d875d
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-10-18 14:38:11 +0530

    sorting -> array partition

---
+++ sorting_array_partition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int arrayPairSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+        for (int i = nums.size() - 2; i >= 0; i -= 2)
+            sum += nums[i];
+        return sum;
+    }
+};
