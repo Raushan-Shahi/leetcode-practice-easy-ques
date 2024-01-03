b04e43c1b1cf177736b3a6e06f3d273699a9e629
Raushan-Shahi <raushanshahi2001@gmail.com>
2024-01-03 14:40:25 +0530

    recursion -> fibonacci series

---
+++ recursion_fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0)
+            return 0;
+        else if(n==1)
+            return 1;
+        else
+            return fib(n-1)+fib(n-2);
+    }
+};
+
+int main(){
+    
+return 0;
+}
